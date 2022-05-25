abc3e6f758a91879cd601f86b197565ed0f37c8f
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2022-05-24 22:51:22 -0500

    Add Heroku needed config

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const logger = require('./loggerMiddleware')
 
-const app = express()
+app.use(cors())
+app.use(express.json());
+
+app.use(logger);
 
 /*Lista de usuarios registrados*/
 let users = [
@@ -35,10 +41,50 @@ app.get('/api/users', (request, response) => {
 app.get('/api/users/:id', (request, response) => {
     const id = Number(request.params.id)
     const user = users.find(user => user.id === id)
-    response.json(user)
+
+    if (user) {
+        response.json(user)
+    } else {
+        response.status(404).end()
+    }
 })
 
-const PORT = 3001
+app.delete('/api/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    users = users.filter(user => user.id !== id);
+    response.status(204).end();
+})
+
+app.post('/api/users', (request, response) => {
+    const user = request.body;
+    const ids = users.map(user => user.id);
+    const maxID = Math.max(...ids);
+
+    /*
+    if (!user || !user.content) {
+        return response.status(400).json({
+            error: 'user.content is missing'
+        })
+    }*/
+
+    const newUser = {
+        id: maxID + 1,
+        email: user.email,
+        password: user.password,
+        name: user.email.substring(0, user.email.indexOf("@"))
+    }
+
+    users = [...users, newUser];
+    response.json(newUser);
+})
+
+app.use((request, response) => {
+    response.status(404).json({
+        error: 'Not found'
+    })
+})
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
